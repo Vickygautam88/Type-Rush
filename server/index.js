fdb83ed33f9455ec51cc3290048c49ee18acc0e5
Abhishek <Abhishri512@gmail.com>
2024-03-02 21:36:31 +0530

    socketIO

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// Imports
 const express = require("express");
 const http = require("http");
 const mongoose = require("mongoose");
@@ -6,11 +7,28 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app);
+var io = require("socket.io")(server);
 
 // Middle Ware
 app.use(express.json());
 
 // Connection to MongoDB
+const DB =
+  "mongodb+srv://system:system@cluster0.9bn1st4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Listening to Socket IO
+io.on("Connection", (socket) => {
+  console.log(socket.id);
+});
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 // listen to Server
 server.listen(port, "0.0.0.0", () => {
